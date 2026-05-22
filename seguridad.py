44aa73ebe32380c41678e2f8ca4599e93effdecc
Elmer Jesus <elmerjesusquispesulla@gmail.com>
2026-05-21 19:47:03 -0500

    Aprendiendo Encapsulamiento y métodos de validación

--- seguridad.py
+++ seguridad.py
@@ -1,31 +1,36 @@
 class Usuario:
-    def __init__(self, nombre, email):
+    def __init__(self, nombre, email, password):
         self.nombre=nombre
         self.email=email
+        self.__password=password
+
     def describir(self):
         print(f"Usuario {self.nombre} | Email: {self.email}")
+    
+    def actualizar_password(self, vieja_pass, nueva_pass):
+        if vieja_pass==self.__password:
+            self.__password=nueva_pass
+            print("✅ Contraseña actualizada con éxito.")
+        else:
+            print("❌ Error: La contraseña vieja no coincide. Intento de hackeo detectado.")
 
-class Admin(Usuario):
-    def __init__(self,nombre, email, nivel_acceso):
-        super().__init__(nombre, email)
-        self.nivel_acceso=nivel_acceso
-    def mostrar_privilegios(self):
-        print(f"El admin {self.nombre}tiene acceso nivel:{self.nivel_acceso}")
-
-class Invitado(Usuario):
-    def __init__(self, nombre, email):
-        super().__init__(nombre, email)
-    def describir(self):
-        print("Soy un invitado,no tengo email regsitrado")
-
-#creamos a nuestro ususario
-usuario_comun=Usuario("Elmer", "elmerjesusuquispesulla@gmail.com")
-usuario_comun.describir()
-#creamos a  nuestro admin
-jefe=Admin("Elmer1","elmer@gamil.com","Administrador")
-jefe.describir()
-jefe.mostrar_privilegios()
-#creamos al invitado
-invitado=Invitado("Jorge","sin email")
-invitado.describir()
+    def validar_email(self, email):
+        if "@" in email and email.count("@")==1:
+            print("✅ Correo valido")
+        else:
+            print("❌El correo no es valido")
+    
+# --- PRUEBA DE SEGURIDAD ---
+user_test=Usuario("Elmer", "elmer@gmail.com", "Elmer123")
+# Verificamos si el correo es valido
+user_test.validar_email("elmergmail.com")
+# Intento 1: Error (Contraseña vieja mal)
+user_test.actualizar_password("12345","elmer1234")
+# Intento 2: Éxito
+user_test.actualizar_password("Elmer123","elmer1234")
+# Intento 3: ¿Podemos ver la contraseña directamente? 
+try:
+    print(user_test.__password)
+except AttributeError:
+    print("🛡️ Seguridad activa: No puedes acceder a la contraseña directamente desde afuera.")
 
